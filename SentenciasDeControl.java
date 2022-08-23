7fdbfb9db59151d0cea85b9dea4c97daec84f8bc
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2022-08-23 15:36:25 +0200

    Add switch

--- SentenciasDeControl.java
+++ SentenciasDeControl.java
@@ -47,6 +47,34 @@ public class SentenciasDeControl {
             System.out.println("NumeroFor: " + numeroFor);
         }
         System.out.println("*************************************************");
+        // Ejercicio 5
+        // Por último, para el Switch, deberás crear la variable estacion, y distintos case 
+        // para las cuatro estaciones del año. Dependiendo del valor de la variable estacion 
+        // se deberá mandar un mensaje por consola informando de la estación en la que está. 
+        // También habrá que poner un default para cuando el valor de la variable no sea una estación.
+        final String VERANO = "VERANO";
+        final String PRIMAVERA = "PRIMAVERA";
+        final String OTOÑO = "OTOÑO";
+        final String INVIERNO = "INVIERNO";
+        String estacion = "";
+        switch (estacion) {
+            case VERANO:
+                System.out.println(VERANO);
+                break;
+            case PRIMAVERA:
+                System.out.println(PRIMAVERA);
+                break;
+            case OTOÑO:
+                System.out.println(OTOÑO);
+                break;
+            case INVIERNO:
+                System.out.println(INVIERNO);
+                break;
+                
+                default:
+                System.out.println("No es una estación");                
+                break;
+        }
     }
 
 
