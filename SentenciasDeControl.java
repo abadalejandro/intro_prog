b141d62436717ccc1f0399f2e1ce54deef8fd503
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2022-08-23 15:31:02 +0200

    Add numeroFor

--- SentenciasDeControl.java
+++ SentenciasDeControl.java
@@ -18,7 +18,8 @@ public class SentenciasDeControl {
 
         System.out.println("*************************************************");        
         // Ejercicio 2
-        // Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+        // Crea un bucle While, este bucle tendrá que tener como condición que la variable 
+        // numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
         // Incrementar el valor de la variable en uno cada vez que se ejecute.
         // Mostrarlo por pantalla cada vez que se ejecute.
         
@@ -29,10 +30,22 @@ public class SentenciasDeControl {
         }
         System.out.println("*************************************************");
 
-        // Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+        // Ejercicio 3
+        // Para el bucle Do While, deberás crear la misma estructura que en el While, 
+        // pero solo se debe ejecutar una vez.
         do{
             System.out.println("Numero While: " + numeroWhile);            
         }while(numeroWhile < 3);
+        System.out.println("*************************************************");
+        
+        // Ejercicio 4
+        // Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 
+        // y su condición será que la variable sea igual o menor que 3, se irá incrementando en 
+        // 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+        
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println("NumeroFor: " + numeroFor);
+        }
         System.out.println("*************************************************");
     }
 
