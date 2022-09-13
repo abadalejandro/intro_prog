3b74ed8f057af98848f67fa1eddd3381c9f172ad
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2022-09-13 13:34:48 +0200

    javascript ejercicio 02 => ok

---
+++ Javascript/02-Sintaxis-variables-palabras-reservadas/Ejercicio-02.js
@@ -0,0 +1,11 @@
+const person = ['Marcos', 33, true, new Date('01/01/1980'), {
+    titulo: 'Lord of the Rings', 
+    autor: 'J.R.R. Tolkien',
+    fecha: new Date('07/29/1954'),
+    url: 'https://www.amazon.it/Lord-rings-J-R-Tolkien/dp/0261103253/ref=asc_df_0261103253/?tag=googshopit-21&linkCode=df0&hvadid=194937906479&hvpos=&hvnetw=g&hvrand=4804602353757701568&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=20523&hvtargid=pla-95452485660&psc=1',
+} ];
+
+
+
+console.log(person[3]);
+console.log(person[4].fecha)
---
+++ Javascript/02-Sintaxis-variables-palabras-reservadas/iniciacion-tipos.js
@@ -0,0 +1,21 @@
+// Tipos primitivos
+
+// number
+// 4
+
+// string
+// "Hola mundo";
+// 'Hola mundo';
+// `Hola mundo`;
+
+
+// booleanos
+// true;
+// false;
+
+
+// null y undefined
+// null;
+// undefined;
+// null, undefined, false, 0 => Todas son Falsy
+console.log(null === undefined);
